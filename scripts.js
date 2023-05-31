cb1befcb0e41c326d2a05c5fae2501e6b491e713
Tristenh <tristenhobba1@gmail.com>
2023-05-31 17:37:01 +1000

    added while loop, declared player variable

---
+++ scripts.js
@@ -0,0 +1,27 @@
+let player = userSelection()
+
+function userSelection(){
+    let player = prompt("Please select R, P or S")
+    player = player.toLocaleUpperCase()
+    while (true){
+        if (player === "R"){
+            alert("you have selected Rock")
+            return player
+        }
+        else if (player === "P"){
+            alert ("you have selected Paper")
+           return player
+            
+        }
+        else if (player === "S"){
+            alert("you have selected Scissors")
+            return player
+        }
+        else{ (player !== ("RPS"))
+            alert("invalid selection")
+            userSelection()
+        }
+        
+    } 
+}
+console.log(player)
