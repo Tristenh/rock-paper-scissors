71ea45190a7c01ebb23a16b264afac924d99f55a
Tristenh <tristenhobba1@gmail.com>
2023-05-31 19:21:02 +1000

    changed char array to R, P, S and instead play agin function used a while loop instead

--- scripts.js
+++ scripts.js
@@ -1,60 +1,67 @@
-let player = userSelection()
+while (playAgain = true) {
 
-function userSelection(){
-    let player = prompt("Please select R, P or S")
-    player = player.toLocaleUpperCase()
-    while (true){
-        if (player === "R"){
-            alert("you have selected Rock")
-            return player
+    player = userSelection()
+    pcMove = pcSelection()
+    function userSelection(){
+        let player = prompt("Please select R, P or S")
+        player = player.toLocaleUpperCase()
+        while (true){
+            if (player === "R"){
+                alert("you have selected Rock")
+                return player
+            }
+            else if (player === "P"){
+                alert ("you have selected Paper")
+               return player
+            }
+            else if (player === "S"){
+                alert("you have selected Scissors")
+                return player
+            }
+            else{
+                alert("invalid selection")
+                userSelection()
+            }
+        } 
+    }
+    
+    function pcSelection(){
+        let char = ["R", "P", "S"];
+        let randomIndex = Math.floor(Math.random() * char.length)
+        let pcMove = char[randomIndex]
+        if (pcMove === "R"){
+            alert("Pc has selected Rock")
+            return pcMove
         }
-        else if (player === "P"){
-            alert ("you have selected Paper")
-           return player
+        else if (pcMove === "P"){
+            alert ("Pc has selected Paper")
+           return pcMove
         }
-        else if (player === "S"){
-            alert("you have selected Scissors")
-            return player
+        else if (pcMove === "S"){
+            alert("Pc has selected Scissors")
+            return pcMove
+    }
+    }
+    
+    resutlt()
+    function resutlt (){
+        if (player ==="R" && pcMove === "S"){
+            alert("player wins")
+        }
+        else if (player ==="P" && pcMove === "R"){
+            alert("player wins")
+        }
+        else if (player ==="S" && pcMove === "P"){
+            alert("player wins")
+        }
+        else if (player === pcMove){
+            alert("its a tie")
         }
         else{
-            alert("invalid selection")
-            userSelection()
+            alert("pc wins")
         }
-    } 
-}
-let pcMove = pcSelection()
-function pcSelection(){
-    let char = ["Rock", "Paper", "Scissors"];
-    let randomIndex = Math.floor(Math.random() * char.length)
-    let pcMove = char[randomIndex]
-    alert(`pc has chosen ${pcMove}`)
-    return pcMove
-}
-resutlt()
-function resutlt (){
-    if (player ==="R" && pcMove === "Scissors"){
-        alert("player wins")
     }
-    else if (player ==="P" && pcMove === "Rock"){
-        alert("player wins")
-    }
-    else if (player ==="S" && pcMove === "Paper"){
-        alert("player wins")
-    }
-    else{
-        alert("pc wins")
-    }
-}
-playAgain()
-function playAgain(){
-   let playConfirm = confirm("play again ?")
-    if (playConfirm){
-        userSelection()
-        pcSelection()
-        resutlt()
-        playAgain()
-    }
-    else{
-        return
-    }
+    
+    playAgain  = confirm("play again ?")
+    return
 }
